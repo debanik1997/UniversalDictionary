ceb9df1defaa7c748b4c81dc9ab6761d65e866cc
debanikpurkayastha <debanik1997@gmail.com>
2020-01-09 19:35:39 +0530

    Initial Commit

---
+++ UniversalDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniversalDictionary
+//
+//  Created by Debanik Purkayastha on 1/9/20.
+//  Copyright © 2020 Debanik Purkayastha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
